45fc3723d508b9d6d6e1dbae040bcb4437e077fd
Nathan Istace <nathan.istace@gmail.com>
2025-03-06 10:24:33 +0100

    added target is alive condition to melee attack

---
+++ Assets/Scripts/DiceBotsGame/CombatActions/Conditions/TargetIsAliveCombatConditionAction.cs
@@ -0,0 +1,12 @@
+﻿using DiceBotsGame.CombatGrids;
+using DiceBotsGame.DiceBots;
+using UnityEngine;
+
+namespace DiceBotsGame.CombatActions.Conditions {
+   public class TargetIsAliveCombatActionCondition : MonoBehaviour, ICombatActionCondition {
+      public bool Check(CombatGrid combatGrid, DiceBot actor, CombatGridTile targetTile, int value) {
+         var botAtPosition = combatGrid.GetDiceBotAtPosition(targetTile);
+         return botAtPosition && botAtPosition.HealthSystem.IsAlive;
+      }
+   }
+}
