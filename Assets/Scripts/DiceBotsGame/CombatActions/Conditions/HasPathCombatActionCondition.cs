c150a861d56db0a747186c1baa87905a109fa227
Nathan Istace <nathan.istace@gmail.com>
2025-03-06 10:24:20 +0100

    optimized "has path" condition

--- Assets/Scripts/DiceBotsGame/CombatActions/Conditions/HasPathCombatActionCondition.cs
+++ Assets/Scripts/DiceBotsGame/CombatActions/Conditions/HasPathCombatActionCondition.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 namespace DiceBotsGame.CombatActions.Conditions {
    public class HasPathCombatActionCondition : MonoBehaviour, ICombatActionCondition {
       public bool Check(CombatGrid combatGrid, DiceBot actor, CombatGridTile targetTile, int value) {
+         if (CombatGrid.Distance(combatGrid.GetDiceBotPosition(actor), targetTile.Coordinates) > value) return false;
          return combatGrid.TryGetPath(combatGrid.GetDiceBotPosition(actor), targetTile.Coordinates, out var path) && path.Count - 1 <= value;
       }
    }
